018e5c2fb381929dd4f85d8aed0ec24cf4c32ad0
davehernandez92 <davehgal@gmail.com>
2024-01-25 14:50:00 -0400

    TEST2

--- src/app/Contacto/page.js
+++ src/app/Contacto/page.js
@@ -1,14 +1,65 @@
-import Form from "@/components/form";
+"use client";
+
+import React, { useState } from "react";
+
 import Image from "next/image";
+import { Button } from "@/components/ui/input";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea"
+
 import image from "../../../public/images/contacto.webp";
 import imageM from "../../../public/images/contactoM.jpg";
-import styles from '../ui/contactoPage.module.css'
+import styles from "../ui/components/form.module.css";
+
 
 export default function ContactoPage() {
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(""); // New state for phone number
+  const [projectDetails, setProjectDetails] = useState("");
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePhoneNumberChange = (e) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleProjectDetailsChange = (e) => {
+    setProjectDetails(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      recipientEmail: email,
+      customer: name,
+      phoneNumber: phoneNumber, // Include phone number in the data
+      text: `Nombre: ${name}\nEmail: ${email}\nTelefono: ${phoneNumber}\nDetalles: ${projectDetails}`,
+    };
+    console.log(data);
+
+    // Use the imported handleSubmitLogic function
+    // await handleSubmitLogic(data, toast);
+
+    // Reset form fields
+    setName("");
+    setEmail("");
+    setPhoneNumber("");
+    setProjectDetails("");
+  };
   return (
     <main className="containerP lg:px-15">
       <div className="flex flex-col gap-5 pt-6 md:grid md:grid-cols-2 md:gap-6  ">
-        <div className='flex justify-center'>
+        <div className="flex justify-center">
           <Image
             src={imageM}
             width={500}
@@ -24,7 +75,66 @@ export default function ContactoPage() {
             className={`hidden md:block  ${styles.img__Contacto}`}
           />
         </div>
-        <Form />
+
+        {/* FORM  */}
+
+        <form onSubmit={handleSubmit} className={` pt-5 ${styles.form}`}>
+          <h1> Contáctanos </h1>
+          <div className={styles.formgroup}>
+            <div className="flex flex-col w-full max-w-sm  gap-4 lg:max-w-[30rem]">
+              <Label htmlFor="name">Nombre</Label>
+              <Input
+                type="text"
+                id="name"
+                name=""
+                placeholder="Ingrese su nombre"
+                value={name}
+                onChange={handleNameChange}
+                required
+                className={styles.input}
+              />
+
+              <Label htmlFor="email">Email</Label>
+
+              <Input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Ingrese su correo electrónico"
+                value={email}
+                onChange={handleEmailChange}
+                required
+                className={styles.input}
+              />
+
+              {/* New form group for phone number */}
+
+              <Label htmlFor="phoneNumber">Telefono</Label>
+              <Input
+                type="tel"
+                id="phoneNumber"
+                name="phoneNumber"
+                placeholder="Ingrese su número de teléfono"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+                required
+                className={styles.input}
+              />
+
+              <Label htmlFor="project-details">Cuentame sobre ti</Label>
+              <Textarea
+                id="project-details"
+                name="project-details"
+                placeholder="Escriba detalles sobre su evento"
+                value={projectDetails}
+                onChange={handleProjectDetailsChange}
+                required
+                className={styles.textarea}
+              />
+             
+            </div>
+          </div>
+        </form>
       </div>
     </main>
   );
